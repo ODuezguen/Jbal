ec80a128bb667ae56d1cd6f96e938ed51b33fc7a
Onur Düzgün <77804721+oduzgun@users.noreply.github.com>
2022-12-19 00:09:10 +0100

    Update input_product_matrix.R

--- input_product_matrix.R
+++ input_product_matrix.R
@@ -4,15 +4,13 @@
 
 input_product_matrix <- function(input_matrix) {
   # Calculate the product of the input matrix and its transpose
-  product_matrix <- input_matrix %*% t(input_matrix)
-  
+  product_matrix <- input_matrix %*% t(input_matrix) 
   # Select only the lower triangular elements of the product matrix
   lower_tri <- lower.tri(product_matrix)
   product_matrix <- product_matrix[lower_tri]
-  
+ 
   # Combine the input matrix with the lower triangular elements of the product matrix
   product_matrix <- cbind(input_matrix, product_matrix)
-  
-  # Return the resulting matrix
+
   return(product_matrix)
 }
