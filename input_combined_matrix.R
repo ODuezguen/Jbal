46c26dc79f15bd556c59534e425dfb2fb5b38dc1
Onur Düzgün <77804721+oduzgun@users.noreply.github.com>
2022-12-19 00:08:24 +0100

    Update input_combined_matrix.R

--- input_combined_matrix.R
+++ input_combined_matrix.R
@@ -8,23 +8,19 @@ input_combined_matrix <- function(input_matrix) {
   
   # Calculate the product of the input matrix and its transpose
   product_matrix <- input_matrix %*% t(input_matrix)
-  
   # Select only the lower triangular elements of the product matrix
   lower_tri <- lower.tri(product_matrix)
   product_matrix <- product_matrix[lower_tri]
     
   # Calculate the squares of the values in the identified columns
   square_matrix <- input_matrix[, indices]^2
-  
   # Set the column names of the square matrix to the names of the original columns with "^2" appended
   colnames(square_matrix) <- paste0(colnames(input_matrix)[indices], "^2")
   
   # Combine the product matrix and the square matrix
   combined_matrix <- cbind(product_matrix, square_matrix)
-  
   # Combine the input matrix with the combined matrix
   out <- cbind(input_matrix, combined_matrix)
   
-  # Return the resulting matrix
   return(out)
 }
