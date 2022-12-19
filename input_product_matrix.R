98f051f4543c6ef1799627263f068ff6595b6845
Onur Düzgün <77804721+oduzgun@users.noreply.github.com>
2022-12-19 23:57:30 +0100

    Update input_product_matrix.R

--- input_product_matrix.R
+++ input_product_matrix.R
@@ -3,14 +3,31 @@
 #' The new columns are added only for columns that have more than two unique values in the original matrix.
 
 input_product_matrix <- function(input_matrix) {
-  # Calculate the product of the input matrix and its transpose
-  product_matrix <- input_matrix %*% t(input_matrix) 
-  # Select only the lower triangular elements of the product matrix
+  
+  # Adding some checks
+  if (!is.matrix(input_matrix) || !is.numeric(input_matrix)) {
+    stop("Error: input_matrix must be a numeric matrix.")
+  }
+  if (nrow(input_matrix) < 2) {
+    stop("Error: input_matrix must have at least 2 rows.")
+  }
+  
+  # Calculating the product of the input matrix and its transpose
+  tryCatch({
+    product_matrix <- input_matrix %*% t(input_matrix)
+  }, error = function(e) {
+    stop("Error: An error occurred while attempting to calculate the product of input_matrix and its transpose:", e)
+  })
+  # Selecting the lower triangular elements of the product matrix
   lower_tri <- lower.tri(product_matrix)
   product_matrix <- product_matrix[lower_tri]
- 
-  # Combine the input matrix with the lower triangular elements of the product matrix
-  product_matrix <- cbind(input_matrix, product_matrix)
+  
+  # Combining the input matrix with the lower triangular elements of the product matrix
+  tryCatch({
+    product_matrix <- cbind(input_matrix, product_matrix)
+  }, error = function(e) {
+    stop("Error: An error occurred while attempting to bind input_matrix and product_matrix:", e)
+  })
 
   return(product_matrix)
 }
