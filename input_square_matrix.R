f147804c37d74451f78350a7f272bb12bdc4e572
Onur Düzgün <77804721+oduzgun@users.noreply.github.com>
2022-12-20 00:05:48 +0100

    Update input_square_matrix.R

--- input_square_matrix.R
+++ input_square_matrix.R
@@ -3,16 +3,27 @@
 #' The new columns are added only for columns that have more than two unique values in the original matrix.
 
 input_square_matrix <- function(input_matrix) {
-  # Identify the columns of the input matrix that have more than two unique values
-  indices <- which(sapply(input_matrix, function(x) length(unique(x)) > 2))
   
-  # Calculate the squares of the values in the identified columns
+  # Adding some checks
+  if (!is.matrix(input_matrix) || !is.numeric(input_matrix)) {
+    stop("Error: input_matrix must be a numeric matrix.")
+  }
+  if (nrow(input_matrix) < 2) {
+    stop("Error: input_matrix must have at least 2 rows.")
+  }
+  
+  # Identifying the columns of the input matrix that have more than two unique values
+  indices <- which(sapply(input_matrix, function(x) length(unique(x)) > 2))
+  # Calculating the squares of the values in the identified columns
   square_matrix <- input_matrix[, indices]^2
-  # Set the column names of the square matrix to the names of the original columns with ".2" appended
   colnames(square_matrix) <- paste0(colnames(input_matrix)[indices], "^2")
   
-  # Combine the input matrix with the square matrix
-  out <- cbind(input_matrix, square_matrix)
+  # Combining the input matrix with the square matrix
+  tryCatch({
+    out <- cbind(input_matrix, square_matrix)
+  }, error = function(e) {
+    stop("Error: An error occurred while attempting to bind input_matrix and square_matrix:", e)
+  })
   
   return(out)
 }
