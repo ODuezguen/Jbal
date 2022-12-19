24cf7bd0c1df74226240f9c35de2fad12608ccd7
Onur Düzgün <77804721+oduzgun@users.noreply.github.com>
2022-12-19 23:46:55 +0100

    Update cov.balcheck.R

--- cov.balcheck.R
+++ cov.balcheck.R
@@ -11,15 +11,39 @@
 
 
 cov.balcheck <- function(matchbal_output, variable_names, after_matching = TRUE) {
-  # Use the select function to select only the columns needed in the output matrix
-  output_matrix <- ifelse(after_matching == FALSE, 
-                         select(matchbal_output$BeforeMatching, mean.Tr, mean.Co, sdiff, sdiff.pooled, 
-                         var.ratio, p.value, ks.boot.pvalue = ks$ks.boot.pvalue, meandiff = qqsummary$meandiff, 
-                         mediandiff = qqsummary$mediandiff, maxdiff = qqsummary$maxdiff),
-                         select(matchbal_output$AfterMatching, mean.Tr, mean.Co, sdiff, sdiff.pooled, 
-                         var.ratio, p.value, ks.boot.pvalue = ks$ks.boot.pvalue, meandiff = qqsummary$meandiff, 
-                         mediandiff = qqsummary$mediandiff, maxdiff = qqsummary$maxdiff))
-  # Set the column names and row names
+  
+  # Adding some checks
+  if (!is.list(matchbal_output) || length(matchbal_output) != 2) {
+    stop("Error: matchbal_output must be a list with two elements.")
+  }
+  if (!is.character(variable_names) || length(variable_names) != nrow(output_matrix)) {
+    stop("Error: variable_names must be a character vector with the same number of elements as the number of rows in output_matrix.")
+  }
+  if (!is.logical(after_matching)) {
+    stop("Error: after_matching must be a logical value.")
+  }
+  
+  # Selecting the columns needed in the output matrix
+  tryCatch({
+    output_matrix <- ifelse(after_matching == FALSE, 
+                           select(matchbal_output$BeforeMatching, mean.Tr, mean.Co, sdiff, sdiff.pooled, 
+                           var.ratio, p.value, ks.boot.pvalue = ks$ks.boot.pvalue, meandiff = qqsummary$meandiff, 
+                           mediandiff = qqsummary$mediandiff, maxdiff = qqsummary$maxdiff),
+                           select(matchbal_output$AfterMatching, mean.Tr, mean.Co, sdiff, sdiff.pooled, 
+                           var.ratio, p.value, ks.boot.pvalue = ks$ks.boot.pvalue, meandiff = qqsummary$meandiff, 
+                           mediandiff = qqsummary$mediandiff, maxdiff = qqsummary$maxdiff))
+  }, error = function(e) {
+    stop("Error: An error occurred while attempting to select columns from matchbal_output:", e)
+  })
+  
+  # Checking that the necessary columns exist in matchbal_output$BeforeMatching or matchbal_output$AfterMatching
+  if (!all(c("mean.Tr", "mean.Co", "sdiff", "sdiff.pooled", "var.ratio", "p.value") %in% colnames(output_matrix)) || 
+      !all(c("ks.boot.pvalue", "meandiff", "mediandiff", "maxdiff") %in% names(ks)) || 
+      !all(c("meandiff", "mediandiff", "maxdiff") %in% names(qqsummary))) {
+    stop("Error: The necessary columns or elements do not exist in matchbal_output or ks/qqsummary.")
+  }
+  
+  # Setting the column names and row names
   colnames(output_matrix) <- c("mean_treatment", "mean_control", "sdiff", "sdiff_pooled", 
                                "var_ratio", "T_pval", "KS_pval", "qqmeandiff", "qqmediandiff", "qqmaxdiff")
   rownames(output_matrix) <- variable_names
