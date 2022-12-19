39172fcde3eb22c8f6f858adf551d92ab5502714
Onur Düzgün <77804721+oduzgun@users.noreply.github.com>
2022-12-19 23:54:06 +0100

    Update input_combined_matrix.R

--- input_combined_matrix.R
+++ input_combined_matrix.R
@@ -3,23 +3,44 @@
 #' as well as the squares of the values.
 
 input_combined_matrix <- function(input_matrix) {
-  # Identify the columns of the input matrix that have more than two unique values
+  # Adding some checks
+  if (!is.matrix(input_matrix) || !is.numeric(input_matrix)) {
+    stop("Error: input_matrix must be a numeric matrix.")
+  }
+  if (nrow(input_matrix) < 2) {
+    stop("Error: input_matrix must have at least 2 rows.")
+  }
+  
+  # Identifying the columns of the input matrix that have more than two unique values
   indices <- which(sapply(input_matrix, function(x) length(unique(x)) > 2))
   
-  # Calculate the product of the input matrix and its transpose
-  product_matrix <- input_matrix %*% t(input_matrix)
-  # Select only the lower triangular elements of the product matrix
+  # Calculating the product of the input matrix and its transpose
+  tryCatch({
+    product_matrix <- input_matrix %*% t(input_matrix)
+  }, error = function(e) {
+    stop("Error: An error occurred while attempting to calculate the product of input_matrix and its transpose:", e)
+  })                       
+  # Selecting only the lower triangular elements of the product matrix
   lower_tri <- lower.tri(product_matrix)
   product_matrix <- product_matrix[lower_tri]
     
-  # Calculate the squares of the values in the identified columns
+  # Calculating the squares of the values in the identified columns
   square_matrix <- input_matrix[, indices]^2
   # Set the column names of the square matrix to the names of the original columns with "^2" appended
   colnames(square_matrix) <- paste0(colnames(input_matrix)[indices], "^2")
   
-  # Combine the product matrix and the square matrix
-  combined_matrix <- cbind(product_matrix, square_matrix)
-  # Combine the input matrix with the combined matrix
+  # Verifying that the square_matrix has at least one column
+  if (ncol(square_matrix) == 0) {
+    stop("Error: square_matrix must have at least one column.")
+  }
+  
+  # Combining the product matrix and the square matrix
+  tryCatch({
+    combined_matrix <- cbind(product_matrix, square_matrix)
+  }, error = function(e) {
+    stop("Error: An error occurred while attempting to bind product_matrix and square_matrix:", e)
+  })
+  # Combining the input matrix with the combined matrix
   out <- cbind(input_matrix, combined_matrix)
   
   return(out)
